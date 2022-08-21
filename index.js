12998dbfea1d778938fa0e9597da0934a3a02f62
febrian nugroho <fbrnngrh@gmail.com>
2022-08-21 10:45:07 +0800

    add function

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ function getDate() {
     let year = date.getFullYear();
     return day + "/" + month + "/" + year;
 }
+// make function to get the current time
+function getTime() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return day + "/" + month + "/" + year;
+}
+// make function to get the current date and time
+function getDateTime() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    return day + "/" + month + "/" + year + " " + hour + ":" + minute + ":" + second;
+}
 
-console.log(getDate());
+console.log(getDateTime());
