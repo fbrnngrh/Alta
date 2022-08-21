d47c34c3c9aba76e7fba96d25aae22ecd447b7d9
febrian nugroho <fbrnngrh@gmail.com>
2022-08-21 10:49:20 +0800

    remove unusable function

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ function getDate() {
     let year = date.getFullYear();
     return day + "/" + month + "/" + year;
 }
-// make function to get the current time
-function getTime() {
-    let date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    return day + "/" + month + "/" + year;
-}
+
 // make function to get the current date and time
 function getDateTime() {
     let date = new Date();
